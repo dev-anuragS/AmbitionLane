f9bc2fde090fdfd3165efb64602b58d5f66b7d98
Anurag <89013723+dev-anuragS@users.noreply.github.com>
2022-08-21 18:58:45 +0530

    changing phone number

--- ambition-lane-lp/src/components/helperComponents/PhoneButton.jsx
+++ ambition-lane-lp/src/components/helperComponents/PhoneButton.jsx
@@ -4,8 +4,8 @@ import '../css/phoneButtonCSS.css'
 const PhoneButton = () => {
     return (
         <>
-            <a href='tel:+916306000929' id='onHoverTag'><button id='phoneButton'><i className="fas fa-phone"></i></button>
-            <p id="textOnHover">+916306000929</p></a>
+            <a href='tel:+917210978750' id='onHoverTag'><button id='phoneButton'><i className="fas fa-phone"></i></button>
+            <p id="textOnHover">+917210978750</p></a>
         </>
     )
 }
